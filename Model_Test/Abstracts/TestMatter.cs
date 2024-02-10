91a1333815a9dad9feb95c7f3c0a83b191f9ccc1
Leonid Bushuev <leonidos@gmail.com>
2024-02-11 00:41:15 +0100

    Introduce Model Test

---
+++ Model_Test/Abstracts/TestMatter.cs
@@ -0,0 +1,54 @@
+using Model.Essence.Abstracts;
+using Model.Imp.Abstracts;
+
+namespace Model.Test.Abstracts;
+
+
+public interface TestYard : MediumMatter
+{
+    public NamingFamily<TestRabbit> Rabbits { get; }
+    public NamingFamily<TestGuineaPig> GuineaPigs { get; }
+}
+
+
+public interface TestRabbit : NamedTermMatter
+{
+    
+}
+
+
+public interface TestGuineaPig : NamedTermMatter
+{
+    
+}
+
+
+
+public class ImmTestYard : ImmMatter, TestYard
+{
+    public /*Imm*/NamingFamily<TestRabbit> Rabbits { get; }
+    public /*Imm*/NamingFamily<TestGuineaPig> GuineaPigs { get; }
+
+    public ImmTestYard(uint id, /*Imm*/NamingFamily<TestRabbit> rabbits, /*Imm*/NamingFamily<TestGuineaPig> guineaPigs) : base(id)
+    {
+        Rabbits = rabbits;
+        GuineaPigs = guineaPigs;
+    }
+
+}
+
+
+public class ImmTestRabbit : ImmNamedMatter, TestRabbit
+{
+    public ImmTestRabbit(uint id, string name) : base(id, name)
+    {
+    }
+}
+
+
+public class ImmTestGuineaPig : ImmNamedMatter, TestGuineaPig
+{
+    public ImmTestGuineaPig(uint id, string name) : base(id, name)
+    {
+    }
+}
