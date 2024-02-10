d5b3ca232572886d1945a92c7bda14428bf3cde8
Leonid Bushuev <leonidos@gmail.com>
2024-02-11 00:39:01 +0100

    Introduce Model Imp

---
+++ Model_Imp/Abstracts/ImmFamily.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using Model.Essence.Abstracts;
+
+namespace Model.Imp.Abstracts;
+
+/// <summary>
+/// Abstract immutable family.
+/// </summary>
+/// <typeparam name="M">matter type.</typeparam>
+public class ImmFamily<M> : Family<M> 
+    where M: Matter
+{
+    private protected readonly M[] matters;
+
+    
+    public static ImmFamily<M> of(params M[] matters) => new ImmFamily<M>(matters);
+
+    public ImmFamily(M[] matters)
+    {
+        this.matters = matters;
+    }
+
+    
+    public IEnumerator<M> GetEnumerator() => matters.AsEnumerable().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => matters.GetEnumerator();
+
+    public int Count => matters.Length;
+}
+
+
+
+public class ImmNamingFamily<M> : ImmFamily<M>, NamingFamily<M> 
+    where M : NamedMatter
+{
+    public new static ImmNamingFamily<M> of(params M[] matters) => new ImmNamingFamily<M>(matters);
+    
+    public ImmNamingFamily(M[] matters) 
+        : base(matters)
+    {
+    }
+
+    public M? this[string name]
+    {
+        get
+        {
+            foreach (M m in matters)
+                if (m.Name == name) return m;
+            return default(M?);
+        }
+    }
+}
