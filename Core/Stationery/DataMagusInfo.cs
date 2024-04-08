41c67c83f6c3179df6af372c0b31147f87493ece
Leonid Bushuev <leonidos@gmail.com>
2024-04-08 14:33:30 +0200

    Introduce Core

---
+++ Core/Stationery/DataMagusInfo.cs
@@ -0,0 +1,13 @@
+namespace Core.Stationery;
+
+/// <summary>
+/// Top-level information about the software.
+/// </summary>
+public class DataMagusInfo
+{
+    /// <summary>
+    /// Current version.
+    /// </summary>
+    public readonly Version SoftwareVersion = new Version(0, 1);
+
+}
