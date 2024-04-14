d05db48ceffeb534172a90420cabc40d7169e520
Leonid Bushuev <leonidos@gmail.com>
2024-04-14 21:22:40 +0200

    Util: more bytes

--- Util/Fun/NumberConstants.cs
+++ Util/Fun/NumberConstants.cs
@@ -16,6 +16,26 @@ public static class NumberConstants
     public const byte _7_ = 7;
     public const byte _8_ = 8;
     public const byte _9_ = 9;
+    public const byte _10_ = 10;
+    public const byte _11_ = 11;
+    public const byte _12_ = 12;
+    public const byte _13_ = 13;
+    public const byte _14_ = 14;
+    public const byte _15_ = 15;
+    public const byte _16_ = 16;
+    public const byte _17_ = 17;
+    public const byte _18_ = 18;
+    public const byte _19_ = 19;
+    public const byte _20_ = 20;
+    public const byte _21_ = 21;
+    public const byte _22_ = 22;
+    public const byte _23_ = 23;
+    public const byte _24_ = 24;
+    public const byte _25_ = 25;
+    public const byte _26_ = 26;
+    public const byte _27_ = 27;
+    public const byte _28_ = 28;
+    public const byte _29_ = 29;
 
     public const byte _254_ = 254;
     public const byte _255_ = 255;
