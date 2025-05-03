c7f7ce4223e76fada0bca2b8706ad62a6e2b2333
Leonid Bushuev <leonidos@gmail.com>
2025-05-03 22:26:06 +0200

    DataMagus.App: introduce the separate starting module

---
+++ DataMagus_App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataMagus_App;
+
+class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        Gui.Application.Program.Main(args);
+    }
+}
