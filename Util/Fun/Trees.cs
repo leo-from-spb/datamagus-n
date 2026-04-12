aa66d8fb3b5b22382cba1e16677732ebe9a1bd7e
Leonid Bushuev <leonidos@gmail.com>
2026-04-12 21:50:37 +0200

    Trees: Traverse depth-first children-first, add comments and improve tests

--- Util/Fun/Trees.cs
+++ Util/Fun/Trees.cs
@@ -4,9 +4,22 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Util.Fun;
 
+/// <summary>
+/// Routines for working with trees.
+/// </summary>
 public static class Trees
 {
 
+    /// <summary>
+    /// Traverses the tree in depth-first order (the most common way to traverse a tree).
+    /// First, yields the given root node.
+    /// For every node, it yields the node itself and then its children;
+    /// in other words, when a node is yielded, its parent, grandparent, etc. have already been yielded.
+    /// </summary>
+    /// <param name="root">the root to start traverse from.</param>
+    /// <param name="children">a function that provides with children of a tree node.</param>
+    /// <typeparam name="T">type of the tree node.</typeparam>
+    /// <returns>The traversing sequence.</returns>
     public static IEnumerable<T> TraversDepthFirst<T>(T root, Func<T, IEnumerable<T>> children)
         where T: notnull
     {
@@ -21,7 +34,41 @@ public static class Trees
         }
     }
     
-    
+    /// <summary>
+    /// Traverses the tree in depth-first order, but yields children before parents.
+    /// For each node, it processes all children before yielding this node.
+    /// So, the resulting sequence is starting with the first terminal and finishing with the root node.
+    /// </summary>
+    /// <param name="root">the root to start traverse from.</param>
+    /// <param name="children">a function that provides with children of a tree node.</param>
+    /// <typeparam name="T">type of the tree node.</typeparam>
+    /// <returns>The traversing sequence.</returns>
+    public static IEnumerable<T> TraversDepthChildrenFirst<T>(T root, Func<T, IEnumerable<T>> children)
+        where T: notnull
+    {
+        foreach (var node in children(root))
+        {
+            foreach (var child in TraversDepthChildrenFirst(node, children))
+            {
+                yield return child;
+            }
+        }
+
+        yield return root;
+    }
+
+    /// <summary>
+    /// Traverses the tree in breadth-first order.
+    /// First, yields the given root node.
+    /// Then, it yields all direct children of the root.
+    /// Then, it yields the second level of children, etc.
+    /// For every node, it yields the node itself and then its children;
+    /// in other words, when a node is yielded, its parent, grandparent, etc. have already been yielded.
+    /// </summary>
+    /// <param name="root">the root to start traverse from.</param>
+    /// <param name="children">a function that provides with children of a tree node.</param>
+    /// <typeparam name="T">type of the tree node.</typeparam>
+    /// <returns>The traversing sequence.</returns>
     [SuppressMessage("ReSharper", "FunctionRecursiveOnAllPaths")]
     public static IEnumerable<T> TraverseBreadthFirst<T>(T root, Func<T, IEnumerable<T>> children)
         where T: notnull
--- Util_Test/Fun/TreesTest.cs
+++ Util_Test/Fun/TreesTest.cs
@@ -9,12 +9,27 @@ namespace Util.Fun;
 [TestFixture]
 public class TreesTest
 {
+    /// <summary>
+    /// <pre>
+    /// Germany
+    ///     Bayern
+    ///         München
+    ///             Lime
+    ///             Pasing
+    ///             Schwabing
+    ///         Augsburg
+    ///     NRW
+    ///         Köln
+    ///         Düsseldorf
+    ///     Berlin
+    /// </pre>
+    /// </summary>
     private SortedList<string, string[]> SimpleTree
         = new()
           {
-              { "Germany", ["Bayern", "NRW"] },
+              { "Germany", ["Bayern", "NRW", "Berlin"] },
               { "Bayern", ["München", "Augsburg"] },
-              { "München", ["Laim", "Pasing"] },
+              { "München", ["Laim", "Pasing", "Schwabing"] },
               { "NRW", ["Köln", "Düsseldorf"] },
           };
 
@@ -26,17 +41,27 @@ public class TreesTest
         var list = 
             TraversDepthFirst("Germany", s => SimpleTree.Get(s, EmptyStringArray))
                .ToList();
-        List<string> expected = ["Germany", "Bayern", "München", "Laim", "Pasing", "Augsburg", "NRW", "Köln", "Düsseldorf"];
+        List<string> expected = ["Germany", "Bayern", "München", "Laim", "Pasing", "Schwabing", "Augsburg", "NRW", "Köln", "Düsseldorf", "Berlin"];
         list.ShouldBe(expected);
     }
     
+    [Test]
+    public void TraversDepthChildrenFirst_basic()
+    {
+        var list =
+            TraversDepthChildrenFirst("Germany", s => SimpleTree.Get(s, EmptyStringArray))
+               .ToList();
+        List<string> expected = ["Laim", "Pasing", "Schwabing", "München", "Augsburg", "Bayern", "Köln", "Düsseldorf", "NRW", "Berlin", "Germany"];
+        list.ShouldBe(expected);
+    }
+
     [Test]
     public void TraversBreadthFirst_basic()
     {
         var list = 
             TraverseBreadthFirst("Germany", s => SimpleTree.Get(s, EmptyStringArray))
                .ToList();
-        List<string> expected = ["Germany", "Bayern", "NRW", "München", "Augsburg", "Köln", "Düsseldorf", "Laim", "Pasing"];
+        List<string> expected = ["Germany", "Bayern", "NRW", "Berlin", "München", "Augsburg", "Köln", "Düsseldorf", "Laim", "Pasing", "Schwabing"];
         list.ShouldBe(expected);
     }
     
