77e1de1aca655d0330a89a194eb6a35df411542a
Leonid Bushuev <leonidos@gmail.com>
2026-05-23 00:40:47 +0200

    Logging: Add logging to the service masters

--- Core_Imp/Services/CoreServiceMaster.cs
+++ Core_Imp/Services/CoreServiceMaster.cs
@@ -1,6 +1,7 @@
 using Core.Gears.Settings;
 using Core.Interaction.Commands;
 using Core.Stationery;
+using NLog;
 
 namespace Core.Services;
 
@@ -10,23 +11,33 @@ namespace Core.Services;
 public static class CoreServiceMaster
 {
 
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
     public static void Startup()
     {
+        Log.Debug("Core Service Master started");
         BigServiceMill.Init();
         BigServiceMill mill = BigServiceMill.GetTheMill();
 
+
         // instantiate and register all services
+        Log.Debug("Instantiating services...");
         var theSettingsService = mill.Register(new LocalSettingService());
         var theCommandRegistry = mill.Register(new RealCommandRegistry());
 
         // set up the services
+        Log.Debug("Init services...");
         theSettingsService.Init();
         theCommandRegistry.Init();
+
+        Log.Debug("Core services are initialized.");
     }
 
 
     public static void Shutdown()
     {
+        Log.Debug("Core Service Master shutting down...");
+
         var mill = BigServiceMill.GetTheMillWhenInitialized();
         if (mill is not null)
         {
@@ -39,6 +50,8 @@ public static class CoreServiceMaster
             mill.ShutdownAllServices();
             mill.Dispose();
         }
+
+        Log.Debug("Core Service Master shut down complete.");
     }
 
     public static bool IsUp() => BigServiceMill.GetTheMillWhenInitialized() is not null;
--- DataMagus_Main/Services/GuiServiceMaster.cs
+++ DataMagus_Main/Services/GuiServiceMaster.cs
@@ -2,30 +2,38 @@ using Core.Interaction.Commands;
 using Core.Services;
 using DataMagus.Main.Interaction.Commands;
 using DataMagus.Main.Main;
+using NLog;
 
 namespace DataMagus.Main.Services;
 
-
-
 public static class GuiServiceMaster
 {
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
 
     public static void Startup()
     {
+        Log.Debug("Gui Service Master started");
+
         var mill = BigServiceMill.GetTheMill();
 
         // get core services in order for providing them to the newly creating service
         var theCommandRegistry = ServiceMill.GetService<CommandRegistry>();
 
         // instantiate and register all services
+        Log.Debug("Instantiate services...");
         var theGuiCommands       = mill.Register(new SimpleGuiCommands());
         var theKeyboardShortcuts = mill.Register(new KeyboardShortcuts());
-        var theMainMenu          = mill.Register(new MainMenu(theCommandRegistry, theKeyboardShortcuts, MainWindow.Instance));
+        var theMainMenu =
+            mill.Register(new MainMenu(theCommandRegistry, theKeyboardShortcuts,
+                                       MainWindow.Instance));
 
         // setup
+        Log.Debug("Set up services...");
         theGuiCommands.Sunrise(theCommandRegistry);
         theKeyboardShortcuts.Setup();
         theMainMenu.SetupMenu();
-    }
 
+        Log.Debug("Gui services are initialized.");
+    }
 }
